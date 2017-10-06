a6051213e1ccd6a74bcc777e4fd17ffaa6b65394
anishs <anishs@NTPC035>
2017-10-06 16:04:27 +0530

    added data.

--- Project1/ManageData.cpp
+++ Project1/ManageData.cpp
@@ -6,5 +6,8 @@ using namespace std;
 
 namespace ManageData
 {
-	const double LaptopData::gold = 0.000775, LaptopData::mercury=0.000297, LaptopData::copper= 0.038591, LaptopData::lead= 0.001524;
+	//All data is in kg
+	const double LaptopData::gold = 0.000775, LaptopData::mercury = 0.000297, LaptopData::copper = 0.078591,
+	LaptopData::steel = 0.035, LaptopData::lithium = 0.007, LaptopData::lead = 0.001524, LaptopData::carbon = 0.025,
+	LaptopData::nickel = 0.001024, LaptopData::aluminum = 0.215, LaptopData::magnesium = 0.525, LaptopData::electrolyte = 0.055;
 }
--- Project1/ManageData.h
+++ Project1/ManageData.h
@@ -5,10 +5,7 @@ namespace ManageData
 {
 	class LaptopData
 	{
-	public:
-		//All data is in kg
-		static const double gold, silver, copper, mercury,steel, aluminum, lithium, lead;
-
-
+	public:		
+		static const double gold, silver, copper, mercury,steel, aluminum, lithium, lead, nickel, magnesium, carbon, electrolyte;
 	};
 }
