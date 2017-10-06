4c21fe9f556c1a792ad271117760835219f131ea
anishs <anishs@NTPC035>
2017-10-06 16:24:22 +0530

    added data.

--- Project1/ManageData.h
+++ Project1/ManageData.h
@@ -3,9 +3,45 @@
 
 namespace ManageData
 {
+	//These are constant data storing classes
 	class LaptopData
 	{
 	public:		
-		static const double gold, silver, copper, mercury,steel, aluminum, lithium, lead, nickel, magnesium, carbon, electrolyte, plastics, glass, silicon;
+		static const double gold, silver, copper, mercury,steel, aluminum, lead,
+		lithium, nickel, magnesium, carbon, electrolyte, plastics, glass, silicon;
 	};
+	class MobileData
+	{
+	public:
+		static const double gold, silver, copper, mercury, steel, aluminum, lead,
+		lithium, nickel, magnesium, carbon, electrolyte, plastics, glass, silicon;
+	};
+	class TabletData
+	{
+	public:
+		static const double gold, silver, copper, mercury, steel, aluminum, lead,
+		lithium, nickel, magnesium, carbon, electrolyte, plastics, glass, silicon;
+	};
+	class PCData
+	{
+	public:
+		static const double gold, silver, copper, mercury, steel, aluminum, lead,
+		lithium, nickel, magnesium, carbon, electrolyte, plastics, glass, silicon;
+	};
+	//This class stores aggregate data values for one user before they're written to a file
+	class GlobalData
+	{
+	public:
+		static const double gold, silver, copper, mercury, steel, aluminum, lead,
+		lithium, nickel, magnesium, carbon, electrolyte, plastics, glass, silicon;
+	};
+	//This class contains data checking and value assigning functions
+	class DataManip
+	{
+	public:
+		//function checkdata()
+		//function assignvalue()
+		//other functions
+	};
+
 }
